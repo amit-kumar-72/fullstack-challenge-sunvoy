89e4dcf52a53264e5795c940b68fd94fc7e64943
amit-kumar-72 <amitk1872002@gmail.com>
2025-06-10 01:14:20 +0530

    fetching users data completly

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import os from "os"
+import fs from "fs"
 import axios from "axios"
 import { wrapper } from "axios-cookiejar-support";
 import { CookieJar } from "tough-cookie";
@@ -39,6 +39,31 @@ async function loginAndGetUsers() {
     );
      if (loginResponse.status !== 302) throw new Error("Login failed");
     console.log("user  Login successful");
+
+
+const usersResponse = await client.post(
+      "https://challenge.sunvoy.com/api/users",
+      null,
+      {
+        headers: {
+          "User-Agent": "Mozilla/5.0",
+          "Content-Type": "application/json",
+          "Content-Length": "0",
+          "Accept": "application/json, text/javascript, */*; q=0.01",
+          "X-Requested-With": "XMLHttpRequest",
+          "Referer": "https://challenge.sunvoy.com/list"
+        }
+      }
+    );
+
+    if (!Array.isArray(usersResponse.data)) throw new Error("Unexpected users response");
+    console.log(` Retrieved ${usersResponse.data.length} users`);
+
+    fs.writeFileSync("output.json", JSON.stringify(usersResponse.data, null, 2));
+    console.log(" Data saved to output.json");
+
+
+
 }
 catch (error) {
     console.error(" Error:", error.message || error);
